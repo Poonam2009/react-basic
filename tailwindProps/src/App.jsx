ce65725be43336a9e6594bcf5c3b81fcef370a6c
Poonam Rawat <rawatp2009@gmail.com>
2025-01-22 00:06:38 +0530

    change bg color app

--- tailwindProps/src/App.jsx
+++ tailwindProps/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
      <h1 className='text-3xl bg-green-500 p-3 rounded-md'> Vite with Tailwind</h1>
      <Card username = "Poonam" post='Staff Engineer'/>
+     
      <Card username='Json' post='QA Engineer.'/>
      <Card />
     </>
--- tailwindProps/src/components/Card.jsx
+++ tailwindProps/src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card({username = "HC", post = "Not Assigned Yet"}) {
       {/* <img src='https://images.pexels.com/photos/29877248/pexels-photo-29877248/free-photo-of-aerial-view-of-stunning-icelandic-waterfall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='' />
         <h1 classNameName='text-2xl bg-green-500 p-3 rounded'>A card for Photo</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p> */}
-      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mb-4">
         <img
           className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
           src="https://images.pexels.com/photos/29877248/pexels-photo-29877248/free-photo-of-aerial-view-of-stunning-icelandic-waterfall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
